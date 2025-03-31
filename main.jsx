dd7523b68751f3a602301b292116a459edef287f
C33Raven <craigparsons33@comcast.net>
2025-03-31 12:53:41 -0500

    Fresh build with Netlify config
    Fresh build with Netlify config

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PrayerPage from './PrayerPage';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PrayerPage />
-  </React.StrictMode>
-);
